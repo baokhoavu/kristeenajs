a0b34e025d58799c828c279e88b7d10cbd5aacd7
Khoa <tealkrysta92@gmail.com>
2020-12-15 16:51:12 -0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>
-                All donations go towards Kristeena Tranh's Gaming Career
+                All donations go towards Kristeena Tranh's Espots Gaming Career
                 <div className="footer__left">
                     <a href="https://github.com/baokhoavu/kristeenajs">
                         Github
